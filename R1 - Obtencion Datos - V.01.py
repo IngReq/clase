165f857c315b51050d2bf3f76e338eddf1d070da
MaricheloGv <maryichelo1017@gmail.com>
2018-09-12 17:01:26 -0500

    Actualización del archivo para bajar los datos
    La obtención se da mediante la lectura de los identificadores de archivo const.cvs

--- R1 - Obtencion Datos - V.01.py
+++ R1 - Obtencion Datos - V.01.py
@@ -8,5 +8,12 @@ import time
 start = dt.datetime(2007, 1, 1)
 end = dt.datetime(2018, 2, 19)
 
-df = web.DataReader(AAL, 'yahoo', start, end)
-df.to_csv('data/' + reg[0] + '.csv')
+with open('const.csv') as csvarchivo:
+    entrada = csv.reader(csvarchivo)
+    for reg in entrada:
+        print(reg[0])
+        try:
+            df = web.DataReader(reg[0], 'yahoo', start, end)
+            df.to_csv('data/' + reg[0] + '.csv')
+        except RemoteDataError:
+            continue
